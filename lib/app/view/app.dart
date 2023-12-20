b13204807f5132916cca0735f0a4b846c3b0a38b
Paulino Fonseca <paulinofonseca@gmail.com>
2023-12-20 04:38:48 +0100

    fix

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gestao_eventos/counter/counter.dart';
 import 'package:gestao_eventos/l10n/l10n.dart';
 
 class App extends StatelessWidget {
@@ -16,7 +15,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const Placeholder(),
     );
   }
 }
