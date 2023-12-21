5ab710bb8c5616b7e777c012aac5f13133723e63
Paulino Fonseca <paulinofonseca@gmail.com>
2023-12-21 12:10:45 +0100

    feat: adicionar pasta de UC interfaces

--- lib/domain/usecases/produto_usecases.dart
+++ lib/domain/usecases/produto_usecases.dart
@@ -1,16 +1,8 @@
 import 'package:gestao_eventos/domain/entities/produto.dart';
 import 'package:gestao_eventos/domain/repositories/i_produto_repository.dart';
-
-abstract class IProdutoUseCases {
-  Future<List<Produto>> getProdutos();
-  Future<Produto> getProduto(int id);
-  Future<bool> createProduto(Produto produto);
-  Future<Produto> updateProduto(Produto produto);
-  Future<bool> deleteProduto(Produto produto);
-}
+import 'package:gestao_eventos/domain/usecases_interfaces/i_produto_usecase.dart';
 
 class ProdutoUseCases implements IProdutoUseCases {
-
   ProdutoUseCases(this._produtoRepository);
 
   final IProdutoRepository _produtoRepository;
