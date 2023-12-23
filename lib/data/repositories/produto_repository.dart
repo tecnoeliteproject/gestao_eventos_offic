15c913e312ecad1ae34ec3988aafd0883866b3cd
Paulino Fonseca <paulinofonseca@gmail.com>
2023-12-23 16:13:15 +0100

    implementa evento domain

--- lib/data/repositories/produto_repository.dart
+++ lib/data/repositories/produto_repository.dart
@@ -1,7 +1,7 @@
 import 'package:gestao_eventos/data/datasources/i_produto_datasource.dart';
 import 'package:gestao_eventos/data/models/produto_model.dart';
 import 'package:gestao_eventos/domain/entities/produto.dart';
-import 'package:gestao_eventos/domain/repositories/i_produto_repository.dart';
+import 'package:gestao_eventos/domain/repositories/i_evento_repository.dart';
 
 class ProdutoRepository extends IProdutoRepository {
   ProdutoRepository(this._produtoDataSource);
