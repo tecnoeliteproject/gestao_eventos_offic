631a3081f5490f8f927e22b82858f4b294075254
VenceslauCassinda <venceslaucassinda@gmail.com>
2024-02-27 22:07:33 +0100

    running with new dart sdk

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:gestao_eventos/main.dart';
 
-import 'package:gestao_eventos_offic/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
