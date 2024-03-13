362c27cab1cf946643e8b700e7844aca3e5ce2f3
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-03-13 21:38:27 +0100

    adiciona a capacidade de rodar pelo flavor production

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,21 @@
-import 'package:gestao_eventos/presentation/app/app.dart';
-import 'package:gestao_eventos/bootstrap.dart';
+import 'dart:async';
 
-void main() {
-  bootstrap(() => const App());
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gestao_eventos/bootstrap.dart';
+import 'package:gestao_eventos/core/dependences/get_it.dart';
+import 'package:gestao_eventos/firebase_options.dart';
+import 'package:gestao_eventos/presentation/app/app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+  await setupGetIt();
+
+  unawaited(bootstrap(() => const App()));
 }
