4fcff779b227ca2e605f3e22f156f88816697a60
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-03-13 21:35:38 +0100

    resolve o firebase no android

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -18,11 +18,10 @@ import 'package:gestao_eventos/presentation/app/app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (kIsWeb) {
+
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-  }
 
   await setupGetIt();
 
