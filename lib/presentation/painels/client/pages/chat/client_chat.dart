2ea4f050c617d09c93ef98a5151c301a036b1e1a
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-03-22 16:39:51 +0100

    refatoracao

--- lib/presentation/painels/client/pages/chat/client_chat.dart
+++ lib/presentation/painels/client/pages/chat/client_chat.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:gestao_eventos/core/helpers/constants.dart';
 import 'package:gestao_eventos/domain/entities/chat_message.dart';
 import 'package:gestao_eventos/presentation/painels/admin/product_details/product_details.dart';
 import 'package:gestao_eventos/presentation/painels/client/pages/chat/bloc/client_chat_message_bloc.dart';
 
 class ClientChat extends StatefulWidget {
+  const ClientChat({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return _ClientChatState();
@@ -43,7 +44,7 @@ class _ClientChatState extends State<ClientChat> {
               bloc: _bloc,
               builder: (context, state) {
                 if (state is GettingClientMessagesState) {
-                  return Center(child: CircularProgressIndicator());
+                  return const Center(child: CircularProgressIndicator());
                 }
                 if (state is ErrorOnGetClientMessagesState) {
                   return Center(child: Text(state.message));
@@ -53,17 +54,21 @@ class _ClientChatState extends State<ClientChat> {
                 }
 
                 if (messages.isEmpty) {
-                  return Center(child: Text('Sem mensagens'));
+                  return const Center(child: Text('Sem mensagens'));
                 }
                 return ListView.builder(
                   itemCount: messages.length,
                   shrinkWrap: true,
-                  padding: EdgeInsets.only(top: 10, bottom: 10),
-                  physics: NeverScrollableScrollPhysics(),
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  physics: const NeverScrollableScrollPhysics(),
                   itemBuilder: (context, index) {
                     return Container(
-                      padding: EdgeInsets.only(
-                          left: 14, right: 14, top: 10, bottom: 10),
+                      padding: const EdgeInsets.only(
+                        left: 14,
+                        right: 14,
+                        top: 10,
+                        bottom: 10,
+                      ),
                       child: Align(
                         alignment:
                             (messages[index].messageType == MessageType.receiver
@@ -77,10 +82,10 @@ class _ClientChatState extends State<ClientChat> {
                                 ? Colors.grey.shade200
                                 : kChatColor),
                           ),
-                          padding: EdgeInsets.all(16),
+                          padding: const EdgeInsets.all(16),
                           child: Text(
                             messages[index].message,
-                            style: TextStyle(fontSize: 15),
+                            style: const TextStyle(fontSize: 15),
                           ),
                         ),
                       ),
@@ -93,34 +98,36 @@ class _ClientChatState extends State<ClientChat> {
           Align(
             alignment: Alignment.bottomLeft,
             child: Container(
-              margin: EdgeInsets.all(20),
+              margin: const EdgeInsets.all(20),
               height: 60,
               width: double.infinity,
               color: Colors.white,
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: BlocBuilder<ClientChatMessageBloc, ClientChatMessageState>(
+                    child: BlocBuilder<ClientChatMessageBloc,
+                        ClientChatMessageState>(
                       bloc: _bloc,
                       builder: (context, state) {
                         return TextField(
                           controller: _controller,
                           decoration: const InputDecoration(
-                              hintText: 'Escrever mensagem',
-                              hintStyle: TextStyle(color: Colors.black54),
-                              border: InputBorder.none),
+                            hintText: 'Escrever mensagem',
+                            hintStyle: TextStyle(color: Colors.black54),
+                            border: InputBorder.none,
+                          ),
                         );
                       },
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 15,
                   ),
                   BlocBuilder<ClientChatMessageBloc, ClientChatMessageState>(
                     bloc: _bloc,
                     builder: (context, state) {
                       if (state is SendingMessageState) {
-                        return CircularProgressIndicator();
+                        return const CircularProgressIndicator();
                       }
                       if (state is ErrorOnSendMessageState) {
                         return Text(state.message);
@@ -134,18 +141,22 @@ class _ClientChatState extends State<ClientChat> {
                       return FloatingActionButton(
                         onPressed: () {
                           if (_controller.text.isNotEmpty) {
-                            _bloc.add(SendMessageEvent(
-                                message: _controller.text, messages: messages));
+                            _bloc.add(
+                              SendMessageEvent(
+                                message: _controller.text,
+                                messages: messages,
+                              ),
+                            );
                             _controller.clear();
                           }
                         },
-                        child: Icon(
+                        backgroundColor: kPrimaryColor,
+                        elevation: 0,
+                        child: const Icon(
                           Icons.send,
                           color: Colors.white,
                           size: 18,
                         ),
-                        backgroundColor: kPrimaryColor,
-                        elevation: 0,
                       );
                     },
                   ),
