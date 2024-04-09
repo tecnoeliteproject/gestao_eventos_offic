b9a75ed1ce825dc81442fee4687744569c82d76c
VenceslauCassinda <venceslaucassinda@gmail.com>
2024-04-09 21:42:57 +0100

    GDE-58-busca última mensagem para cada conversa

--- lib/core/helpers/date_time.dart
+++ lib/core/helpers/date_time.dart
@@ -15,7 +15,6 @@ class DataTimeHelper {
       return 'Há ${toDay.year - date.year} anos';
     }
   }
-  
 
   static String obterTempoDecorrido(DateTime data) {
     // Calcula a diferença entre a data atual e a data informada.
